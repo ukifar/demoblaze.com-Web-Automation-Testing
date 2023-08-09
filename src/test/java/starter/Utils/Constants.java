acf08678816490e40930622af6a3c2aff3d20ae2
Faruqi Rabbani <faruqirabbani@gmail.com>
2023-08-09 10:58:58 +0700

    Copy template web automation testing

---
+++ src/test/java/starter/Utils/Constants.java
@@ -0,0 +1,81 @@
+package starter.Utils;
+
+public class Constants {
+    public static String BASE_URL = "https://reqres.in";
+    public static final String DIR = System.getProperty("user.dir");
+    public static String REQ_BODY_DIR = DIR+"/src/test/resources/JSON/ReqBody/";
+    public static String JSON_SCHEMA_DIR = DIR+"/src/test/resources/JSON/JSONSchema/";
+
+    //response body line 10-40 Faruqi
+    public static String MESSAGE = "message";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //response body line 41-80 Derin
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
